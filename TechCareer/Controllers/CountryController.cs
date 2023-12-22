88b423da156924c1c1e25a7bd05b0f93d0e3d7f7
Bilge Berivan Zincir <zincirberivan@gmail.com>
2023-12-22 22:31:48 +0300

    get countries

---
+++ TechCareer/Controllers/CountryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get(){
+
+            string[] countries = new string[] {"Turkey", "United States", "Brazil", "Japan", "Germany", "France", "Russia", "China", "India", "Canada",
+             "Australia", "United Kingdom", "Italy", "South Korea", "Mexico", "Spain", "Argentina", "South Africa", "Sweden", "Netherlands"};
+            return countries;
+  
+        }
+    }
+}
