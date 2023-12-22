3ad44812e74d578d967d6dd355c884713f1bde5d
Bilge Berivan Zincir <zincirberivan@gmail.com>
2023-12-22 23:18:36 +0300

    get by numbers

--- TechCareer/Controllers/CountryController.cs
+++ TechCareer/Controllers/CountryController.cs
@@ -7,13 +7,28 @@ namespace TechCareer.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-        [HttpGet]
-        public string[] Get(){
 
-            string[] countries = new string[] {"Turkey", "United States", "Brazil", "Japan", "Germany", "France", "Russia", "China", "India", "Canada",
+        private readonly string[] countries = new string[] {"Turkey", "United States", "Brazil", "Japan", "Germany", "France", "Russia", "China", "India", "Canada",
              "Australia", "United Kingdom", "Italy", "South Korea", "Mexico", "Spain", "Argentina", "South Africa", "Sweden", "Netherlands"};
+        [HttpGet]
+        public string[] Get() {
+
             return countries;
-  
+
         }
+        [HttpGet("{id}")]
+        public ActionResult GetCountries(int id)
+        {
+               if(id<0 || id>= countries.Length)
+               {
+                return BadRequest("Invalid ID");
+
+               }
+               else
+                {
+                return Ok(countries.Take(id).ToArray());
+                }
+          }
+
     }
 }
